4bb0b767e255f8b693a11ea538d9d91113c98857
Jason Sonith <164841606+JasonSonith@users.noreply.github.com>
2025-11-15 00:11:30 -0600

    created hash function and structure of process_chunks function

--- automating-with-python/concurrency_passsword_cracker.py
+++ automating-with-python/concurrency_passsword_cracker.py
@@ -7,10 +7,6 @@ from pathlib import Path
 def create_hash(password):
     hash = hashlib.sha256(password.encode('utf-8'))
     hash = hash.hexdigest()
-    return hash
 
-password = "Jasonpw1234"
-hash = create_hash(password)
-
-print(f"Password is {password}")
-print(f"Hash is {hash}")
+def process_chunks(chunks):
+    pass
